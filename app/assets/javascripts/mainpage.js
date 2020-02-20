93eae160c3696847bd7132ceda2c6632ea184c79
shmulik <shrot@g.jct.ac.il>
2020-02-20 13:58:58 +0200

    scroll page to validation error

--- app/assets/javascripts/mainpage.js
+++ app/assets/javascripts/mainpage.js
@@ -1,6 +1,26 @@
-$(document).on('turbolinks:load',function(){
+$(document).on('turbolinks:load', function () {
+    load_carousel()
+    fix_chrome_fregmant()
+});
+
+function load_carousel() {
     $('.carousel').carousel({
         interval: 5000
     })
-});
+}
+
+function fix_chrome_fregmant() {
+    var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+    if (window.location.hash && isChrome) {
+        setTimeout(function () {
+            var hash = window.location.hash;
+            window.location.hash = "";
+            window.location.hash = hash;
+        }, 300);
+    }
+}
+
+if (document.getElementById("error_field")) {
+    location.hash = '#form';
+}
 // document.addEventListener("turbolinks:load", function() { alert("ok!") });
